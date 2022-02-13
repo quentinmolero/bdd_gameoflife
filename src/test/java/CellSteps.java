cefd308b7c6e71180e308540224f9bd0bcc2988b
Quentin MOLERO <quentin.molero@outlook.com>
2022-02-13 17:23:37 +0100

    Refactored to use context + extract board and grid in test context

--- src/test/java/CellSteps.java
+++ src/test/java/CellSteps.java
@@ -1,66 +1,50 @@
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import io.cucumber.java8.En;
 import org.assertj.core.api.Assertions;
 
-import java.util.Arrays;
-import java.util.List;
+public class CellSteps implements En {
 
-public class CellSteps {
+    public CellSteps(TestContext testContext) {
 
-    List<List<Boolean>> grid = Arrays.asList(
-            Arrays.asList(false, false, false),
-            Arrays.asList(false, false, false),
-            Arrays.asList(false, false, false));
-    Board board;
+        Given("a cell has been dead at the position {int},{int}", (Integer x, Integer y) -> {
+            testContext.grid.get(x).set(y, false);
+        });
 
-    @Given("a cell has been dead at the position {int},{int}")
-    public void aCellHasBeenDeadAtThePosition(int x, int y) {
-        grid.get(x).set(y, false);
-    }
+        Given("a cell has been living at the position {int},{int}", (Integer x, Integer y) -> {
+            testContext.grid.get(x).set(y, true);
+        });
 
-    @Given("a cell has been living at the position {int},{int}")
-    public void aCellHasBeenLivingAtThePosition(int x, int y) {
-        grid.get(x).set(y, true);
-    }
+        And("the cell at position {int},{int} had {int} living neighbours", (Integer x, Integer y, Integer neighbours) -> {
+            int assignedNeighbours = 0;
 
-    @And("the cell at position {int},{int} had {int} living neighbours")
-    public void theCellHadLivingNeighbours(int x, int y, int neighbours) {
-        int assignedNeighbours = 0;
-
-        for (int i = -1; i <= 1; i++) {
-            for (int j = -1; j <= 1; j++) {
-                if (assignedNeighbours < neighbours
-                        && !(i == x && j == y)
-                        && ((x + i) >= 0 && (x + i) < grid.size())
-                        && ((y + j) >= 0 && (y + j) < grid.get(x + i).size())
-                        && !grid.get(x + i).get(y + j)) {
-                    grid.get(x + i).set(y + j, true);
-                    assignedNeighbours++;
+            for (int i = -1; i <= 1; i++) {
+                for (int j = -1; j <= 1; j++) {
+                    if (assignedNeighbours < neighbours
+                            && !(i == x && j == y)
+                            && ((x + i) >= 0 && (x + i) < testContext.grid.size())
+                            && ((y + j) >= 0 && (y + j) < testContext.grid.get(x + i).size())
+                            && !testContext.grid.get(x + i).get(y + j)) {
+                        testContext.grid.get(x + i).set(y + j, true);
+                        assignedNeighbours++;
+                    }
                 }
             }
-        }
-    }
+        });
 
-    @When("the next generation is computed")
-    public void theNextGenerationIsComputed() {
-        board = new Board(grid);
-        grid = board.live();
-    }
+        When("the next generation is computed", () -> {
+            testContext.board = new Board(testContext.grid);
+            testContext.grid = testContext.board.live();
+        });
 
-    @Then("the cell at position {int},{int} should come to life")
-    public void theCellShouldComeToLife(int x, int y) {
-        Assertions.assertThat(grid.get(x).get(y)).isTrue();
-    }
+        Then("the cell at position {int},{int} should come to life", (Integer x, Integer y) -> {
+            Assertions.assertThat(testContext.grid.get(x).get(y)).isTrue();
+        });
 
-    @Then("the cell at position {int},{int} should survive")
-    public void theCellShouldSurvive(int x, int y) {
-        Assertions.assertThat(grid.get(x).get(y)).isTrue();
-    }
+        Then("the cell at position {int},{int} should survive", (Integer x, Integer y) -> {
+            Assertions.assertThat(testContext.grid.get(x).get(y)).isTrue();
+        });
 
-    @Then("the cell at position {int},{int} should die")
-    public void theCellShouldDie(int x, int y) {
-        Assertions.assertThat(grid.get(x).get(y)).isFalse();
+        Then("the cell at position {int},{int} should die", (Integer x, Integer y) -> {
+            Assertions.assertThat(testContext.grid.get(x).get(y)).isFalse();
+        });
     }
 }
---
+++ src/test/java/TestContext.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class TestContext {
+    List<List<Boolean>> grid = Arrays.asList(
+            Arrays.asList(false, false, false),
+            Arrays.asList(false, false, false),
+            Arrays.asList(false, false, false));
+    Board board;
+}
